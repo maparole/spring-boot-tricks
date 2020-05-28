67d05e0c432f013b3b44dbebc41ad0c5e24a8627
Rudy Rakoto <rudy.rakoto@completion.ch>
2020-05-29 00:19:09 +0200

    Actual counters

--- src/main/java/com/example/demo/ControllerAspect.java
+++ src/main/java/com/example/demo/ControllerAspect.java
@@ -10,13 +10,16 @@ import org.springframework.stereotype.Component;
 @Aspect
 public class ControllerAspect {
 	
+	int successCounter = 0;
+	int failureCounter = 0;
+	
     @AfterThrowing ("execution(* com.example.demo.Controller.who(*))")
     public void onError() throws Throwable {
-    	System.out.println("RRRRRRRRH");
+    	System.out.println("RRRRRRRRH " + ++failureCounter);
     }
 
     @AfterReturning(pointcut = "execution(* com.example.demo.Controller.who(*))", returning = "response")
     public void onSuccess(JoinPoint jp, Object response) throws Throwable {
-    	System.out.println("YEAAAAAAH");
+    	System.out.println("YEAAAAAAH " + ++successCounter);
     }
 }
